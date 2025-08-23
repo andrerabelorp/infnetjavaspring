80815626c2bb98be886b5c727fef7e3ef9c231bd
andrerabelorp <61693077+andrerabelorp@users.noreply.github.com>
2025-08-23 08:50:52 -0300

    Start c/ springboot.

---
+++ arabeloapi/src/main/java/br/edu/infnet/arabeloapi/ArabeloapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.arabeloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArabeloapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArabeloapiApplication.class, args);
+	}
+
+}
